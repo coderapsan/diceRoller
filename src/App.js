859ccf0973ee3dcc570e5f13161dd332c782160d
coderapsan <rapsan363@gmail.com>
2023-05-06 10:34:06 +0545

    dice roller completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+ function App() {
+  const[diceState, setDiceState]= React.useState(6)
+  function rollDice(){
+    const randomNumber= Math.floor(Math.random()*6)+1
+    return( setDiceState(randomNumber))
+  }
+ 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <center>
+        <h1 className="titleStyle"> Dice Roller </h1>
+        <img src={require(`./assets/dice${diceState}.png`)} className="imageStyle"></img>
+      <br>
+      </br>
+      <button className="buttonStyle" onClick={rollDice} > Roll the dice </button>
+   
+      </center>
+
+      
+     
+      
     </div>
   );
 }
